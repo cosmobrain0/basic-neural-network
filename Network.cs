6c26ad151910cb108e5d9826ac7cad14ec668fb7
cosmobrain0 <cosmobrain@outlook.com>
2024-06-19 12:42:53 +0100

    first commit

---
+++ Network.cs
@@ -0,0 +1,188 @@
+public class NeuralNetwork
+{
+	public Node[] inputs;
+	public Node[][] hidden;
+	public Node[] outputs;
+
+	public NeuralNetwork(int inputSize, int[] hiddenLayerSizes, int outputSize, Random generator = null, bool random = false)
+	{
+		Func<float, float> activation = x => 1 / (1 + (float)Math.Exp(-x));
+		inputs = new Node[inputSize].Select(_ => new Node(0, 0, activation)).ToArray();
+		hidden = new Node[hiddenLayerSizes.Length][];
+		for (int i=0; i<hiddenLayerSizes.Length; i++)
+			hidden[i] = new Node[hiddenLayerSizes[i]]
+				.Select(_ => new Node(i == 0 ? inputSize : hiddenLayerSizes[i-1], 0, activation))
+				.ToArray();
+		outputs = new Node[outputSize]
+			.Select(_ => new Node(hiddenLayerSizes.Length > 0 ? hiddenLayerSizes[hiddenLayerSizes.Length-1] : inputSize, 0, activation))
+			.ToArray();
+
+		if (random)
+		{
+			for (int i=0; i<hidden.Length; i++)
+			{
+				for (int j=0; j<hidden[i].Length; j++)
+				{
+					for (int k=0; k<hidden[i][j].incomingWeights.Length; k++)
+					{
+						hidden[i][j].incomingWeights[k] = (generator.NextSingle()-0.5f) * 2f;
+					}
+					hidden[i][j].bias = (generator.NextSingle()-0.5f) * 2f;
+				}
+			}
+			for (int i=0; i<outputs.Length; i++)
+			{
+				for (int j=0; j<outputs[i].incomingWeights.Length; j++)
+				{
+					outputs[i].incomingWeights[j] = (generator.NextSingle()-0.5f) * 2f;
+				}
+				outputs[i].bias = (generator.NextSingle()-0.5f) * 2f;
+			}
+		}
+	}
+
+	public float[] Outputs(float[] inputs)
+	{
+		float[] result = inputs;
+		for (int i=0; i<hidden.Length; i++)
+		{
+			float[] newResult = new float[hidden[i].Length];
+			for (int j=0; j<newResult.Length; j++)
+			{
+				newResult[j] = hidden[i][j].Output(result);
+			}
+			result = newResult;
+		}
+		float[] finalResult = new float[outputs.Length];
+		for (int j=0; j<finalResult.Length; j++)
+		{
+			finalResult[j] = outputs[j].Output(result);
+		}
+		return finalResult;
+	}
+
+	public float AverageCost((float[] inputs, float[] idealOutputs)[] testCases)
+	{
+		float[] costs = new float[testCases.Length];
+		Parallel.For(0, testCases.Length, i => {
+			float[] dataPoint = testCases[i].inputs;
+			float[] ideal = testCases[i].idealOutputs;
+			costs[i] = Cost(dataPoint, ideal);
+		});
+		return costs.Sum() / testCases.Length;
+	}
+
+	public void Learn(Random generator, float[] inputs, float[] expectedOutputs, float learnRate)
+	{
+		float originalCost = Cost(inputs, expectedOutputs);
+		float[][][] weightGradients = new float[hidden.Length+1][][]; // for the weights of one node
+
+		Node[][] nodes = hidden.Concat(new Node[][] { outputs }).ToArray();
+		
+		for (int layerCount=0; layerCount < nodes.Length; layerCount++)
+		{
+			Node[] layer = nodes[layerCount];
+			weightGradients[layerCount] = new float[layer.Length][];
+			for (int nodeCount = 0; nodeCount < layer.Length; nodeCount++)
+			{
+				Node node = layer[nodeCount];
+				weightGradients[layerCount][nodeCount] = new float[node.incomingWeights.Length];
+				for (int i=0; i<node.incomingWeights.Length; i++)
+				{
+					float change = (generator.NextSingle()-0.5f) * learnRate * 2;
+					node.incomingWeights[i] += change;
+					float newCost = Cost(inputs, expectedOutputs);
+					weightGradients[layerCount][nodeCount][i] = (newCost-originalCost)/change;
+					node.incomingWeights[i] -= change;
+				}
+			}
+		}
+		float[][] biasGradients = new float[nodes.Length][];
+		for (int i=0; i<nodes.Length; i++)
+		{
+			biasGradients[i] = new float[nodes[i].Length];
+			for (int j=0; j<nodes[i].Length; j++)
+			{
+					float change = (generator.NextSingle()-0.5f) * learnRate * 2;
+					nodes[i][j].bias += change;
+					float newCost = Cost(inputs, expectedOutputs);
+					biasGradients[i][j] = (newCost-originalCost)/change;
+					nodes[i][j].bias -= change;
+			}
+		}
+
+		for (int layerCount=0; layerCount < nodes.Length; layerCount++)
+		{
+			Node[] layer = nodes[layerCount];
+			for (int nodeCount = 0; nodeCount < layer.Length; nodeCount++)
+			{
+				Node node = layer[nodeCount];
+				for (int i=0; i<node.incomingWeights.Length; i++)
+				{
+					node.incomingWeights[i] -= weightGradients[layerCount][nodeCount][i] * learnRate;
+				}
+			}
+		}
+
+		for (int i=0; i<nodes.Length; i++)
+		{
+			for (int j=0; j<nodes[i].Length; j++)
+			{
+					nodes[i][j].bias -= biasGradients[i][j] * learnRate;
+			}
+		}
+		
+	}
+
+	public float Cost(float[] inputs, float[] idealOutputs)
+	{
+		float[] result = Outputs(inputs);
+		float sum = 0f;
+		for (int i=0; i<result.Length; i++) sum += Math.Abs(idealOutputs[i]-result[i]);
+		sum /= idealOutputs.Length;
+		return sum;
+		// return idealOutputs.Select((x, i) => Math.Abs(x-result[i])).Sum() / idealOutputs.Length;
+	}
+}
+
+// TODO: some encapsulation pa-ha-leez
+public class Node
+{
+	public float bias;
+	public float[] incomingWeights;
+	private Func<float, float> activation;
+
+	public bool SetWeight(int i, float weight)
+	{
+		if (i < 0 || i >= incomingWeights.Length) return false;
+		incomingWeights[i] = weight;
+		return false;
+	}
+
+	public Node(int inputs, float bias, Func<float, float> activation)
+	{
+		incomingWeights = new float[inputs];
+		bias = 0;
+		this.activation = activation;
+	}
+
+	/// <summary>
+	/// NOTE: `inputs` better have the right length, or else
+	/// </summary>
+	public float Output(float[] inputs)
+	{
+		float result = 0;
+		for (int i=0; i<incomingWeights.Length; i++)
+		{
+			result += incomingWeights[i] * inputs[i];
+		}
+		return activation(result / inputs.Length + bias);
+	}
+}
+
+struct Connection
+{
+	Node start;
+	Node end;
+	float weight;
+}
---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿Random generator = new Random((int)(DateTime.Now.Ticks << 32 >> 32));
+Random[] generators = new Random[128];
+for (int i=0; i<generators.Length; i++) generators[i] = new Random((int)(generator.NextInt64() << 32 >> 32));
+NeuralNetwork[] networks = new NeuralNetwork[10000];
+
+for (int i=0; i<networks.Length; i++)
+	networks[i] = new NeuralNetwork(2, new int[] {}, 2, generator, true);
+
+// trying to learn to differentiate between stuff below "y = 2x - 1"
+Func<float[], float[]> idealOutputs = inputs => inputs[1] > inputs[0] ? new float[] { 1, 0 } : new float[] { 0, 1 };
+
+Func<float[]> randomDataPoint = () => new float[] { generator.NextSingle(), generator.NextSingle() };
+
+Func<float[], int> highestIndex = values =>
+{
+	int result = 0;
+	for (int i=1; i<values.Length; i++)
+		if (values[i] > values[result]) result = i;
+	return result;
+};
+
+(float[], float[])[] testCases = new (float[], float[])[400];
+for (int j=0; j<testCases.Length; j++)
+{
+	float[] inputs = randomDataPoint();
+	float[] outputs = idealOutputs(inputs);
+	testCases[j] = (inputs, outputs);
+}
+
+// (NeuralNetwork network, float cost)[] result = networks.Select(x => (x, 0.0)).ToArray();
+float[] costs = new float[networks.Length];
+const int learningIterations = 1000;
+for (int i=0; i<learningIterations; i++)
+{
+	if (i%10 == 0) Console.Write($"iteration {i+1} / {learningIterations}: ");
+	// learning
+	// TODO: possible optimisation - manual insertion sort here
+	Parallel.For(0, networks.Length, j => {
+		costs[j] = networks[j].AverageCost(testCases);
+	});
+	// for (int j=0; j<networks.Length; j++)
+	// {
+	// 	float cost = networks[j].AverageCost(testCases);
+	// 	costs[j] = cost;
+	// 	// int k;
+	// 	// for (k=j-1; k>=0; k--)
+	// 	// {
+	// 	// 	if (costs[k] > cost)
+	// 	// 	{
+	// 	// 		networks[k+1] = networks[k];
+	// 	// 		costs[k+1] = costs[k];
+	// 	// 	}
+	// 	// 	else break;
+	// 	// }
+	// 	// costs[k+1] >= cost
+	// 	// costs[k+1] = cost;
+	// 	// networks[k+1] = network;
+	// }
+	Array.Sort(costs, networks);
+	if (i%10 == 0) Console.WriteLine($"Minimum cost - {costs[0]}");
+
+	const float learnRate = 0.23f;
+	Parallel.For(networks.Length/3, networks.Length, j => {
+		Random generator = generators[j%generators.Length];
+		for (int k=0; k<4; k++)
+		{
+			// pick a random node
+			// then pick a random weight or bias
+			// then change it a little
+
+			int nodeIndex = (int)(generator.NextInt64() & 1);
+			int weightIndex = (int)(generator.NextInt64()%3);
+			// int nodeIndex = 1;
+			// int weightIndex = 1;
+
+			// we can use it as a direct index as there are no hidden layers
+			Node node = networks[j].outputs[nodeIndex];
+			float change = (generator.NextSingle()-0.5f) * learnRate; 
+			// float change = 1f * learnRate;
+			if (weightIndex == 2) node.bias += change;
+			else node.incomingWeights[weightIndex] += change;
+		}
+	});
+	// for (int j=networks.Length/10; j<networks.Length; j++)
+	// {
+	// 	const float learnRate = 0.5f;
+	// 	// pick a random node
+	// 	// then pick a random weight or bias
+	// 	// then change it a little
+	// 	int nodeIndex = (int)(generator.NextInt64() & 1);
+	// 	int weightIndex = (int)(generator.NextInt64()%3);
+	// 	// we can use it as a direct index as there are no hidden layers
+	// 	Node node = networks[j].outputs[nodeIndex];
+	// 	float change = (generator.NextSingle()-0.5f) * 2f * learnRate; 
+	// 	if (weightIndex == 2) node.bias += change;
+	// 	else node.incomingWeights[weightIndex] += change;
+	// }
+}
+
+// debug final info
+float minCost = float.PositiveInfinity;
+(float[], float[])[] finalTestCases = new (float[], float[])[100];
+for (int j=0; j<finalTestCases.Length; j++)
+{
+	float[] inputs = randomDataPoint();
+	float[] outputs = idealOutputs(inputs);
+	finalTestCases[j] = (inputs, outputs);
+}
+for (int j=0; j<networks.Length; j++)
+{
+	minCost = Math.Min(minCost, networks[j].AverageCost(finalTestCases));
+}
+Console.WriteLine("min cost - " + minCost);
